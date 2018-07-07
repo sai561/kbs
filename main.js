0052e69b45e9679e099a653ae70fbc441b46b9d9
sai561 <bindusrikothapalli999@gmail.com>
2018-07-07 10:58:50 +0530

    adding file

--- main.js
+++ main.js
@@ -12,4 +12,12 @@ function getfile(file,callback){
 getfile("data.json",function(text){
   var data=JSON.parse(text);
   console.log(data);
+  career(data.career);
 })
+var child2=document.querySelector(".childTwo");
+function career(careerInfo){
+  //console.log(careerInfo);
+var ch=document.createElement("p");
+ch.textContent=careerInfo.info;
+child2.appendChild(ch);
+}
